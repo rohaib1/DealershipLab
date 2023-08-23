9e4bcbf0227a88a93ae842026abb06a02dc22e17
rohaib1 <rohaib192@gmail.com>
2023-08-23 14:34:36 +0100

    Setup

---
+++ Car.js
@@ -0,0 +1,8 @@
+
+function Car(manufacturer, price, engineType){
+    this.manufacturer = manufacturer;
+    this.price = price;
+    this.engineType = engineType;
+}
+
+const car1 = new Car("Toyota", 25000, "Gasoline");
